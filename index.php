da33adbc3410f1d043d3171e38648aff33363d27
takutou <takuto1488@gmail.com>
2024-02-04 00:03:13 +0900

    投稿ページのイラスト表示　タグの作成

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php get_header(); ?>
 <?php if(have_posts()): ?>
-    <article <? post_class();?>>
+    <article <? post_class('');?>>
+        <div>
+            <figure class="figure-area">
+                <?php the_post_thumbnail();?>
+            </figure>
+            
+            <h1><? the_title(); ?></h1>
+            <?php the_content(); ?>
+        </div>
         <time datetime="<?php echo esc_attr(get_the_date(DATE_W3C)); ?>">
                 <?php echo esc_html(get_the_date()); ?>
         </time>
-        <h1><? the_title(); ?></h1>
-        <?php the_content(); ?>
+        
         <?php the_post_navigation(); ?>
     </article>
 <?php endif; ?>
