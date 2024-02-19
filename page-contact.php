bd6eaa9d545fe4c8e69544f071bf07d7858a8675
takutou <takuto1488@gmail.com>
2024-02-19 14:15:02 +0900

    チョークフォント読み込みとコンタクトフォームのデザイン変更

--- page-contact.php
+++ page-contact.php
@@ -3,7 +3,9 @@
     <?php get_header(); ?>
     <main>
         <article>
-            <?php the_content();?>
+            <div class="wp-contents-contact">
+                <?php the_content();?>
+            </div>
             <div class="main-cf7">
                 <?php echo apply_shortcodes( '[contact-form-7 id="070d7bc" title="test"]' ); ?>
             </div>
