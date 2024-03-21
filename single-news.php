7a630e970145341360c8fb1e009d64bd29c0d86c
takutou <takuto1488@gmail.com>
2024-03-21 11:10:58 +0900

    newsページの実装 タイトルと内容を表示

--- single-news.php
+++ single-news.php
@@ -1,5 +1,16 @@
 <div class="anchor base-backcolor">
     <?php get_header(); ?>
-    <div>news php</div>
+    <main style="
+    color: var(--whitechalk);
+    font-family: var(--myfont);
+    text-align: center;
+    
+    ">
+        <h1 style="
+        margin-top: 0px;">
+            <?php the_title(); ?>
+        </h1>
+        <p><?php the_content(); ?></p>
+    </main>
     <?php get_footer(); ?>
 </div>
